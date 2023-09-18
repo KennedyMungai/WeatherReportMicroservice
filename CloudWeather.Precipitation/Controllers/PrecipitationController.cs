ebf96104f1511ea85bf7c921658773aceccda52e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-18 18:11:19 +0300

    Added a null checker for the days value

--- CloudWeather.Precipitation/Controllers/PrecipitationController.cs
+++ CloudWeather.Precipitation/Controllers/PrecipitationController.cs
@@ -11,6 +11,11 @@ public class PrecipitationController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetByZip([FromBody] string zip, [FromQuery] int? days)
     {
+        if (days is null || days > 30 || days < 0)
+        {
+            return await Task.FromResult(BadRequest("Please enter the value for days and it should be less than 30"));
+        }
+
         if (zip is null)
         {
             return await Task.FromResult(BadRequest("The zip code has not been entered"));
