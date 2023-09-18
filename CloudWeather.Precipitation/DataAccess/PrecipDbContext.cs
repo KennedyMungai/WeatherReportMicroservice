314385f03cb8c0c0a9f322d4755238e7c3b72c8b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-18 17:45:36 +0300

    Added a snake case configuration

--- CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -10,4 +10,18 @@ public class PrecipDbContext : DbContext
     }
 
     public DbSet<Precipitation> Precipitations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+
+    public static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Precipitation>(b =>
+        {
+            b.ToTable("precipitation");
+        });
+    }
 }
