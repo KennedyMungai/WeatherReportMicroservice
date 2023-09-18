4bbe6538ffcddb471f959f8ed8d763f88a14cdfa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-18 19:47:02 +0300

    The migration files

---
+++ CloudWeather.Report/DataAccess/Migrations/20230918164633_InitialMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudWeather.Report.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    AverageHighF = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    AverageLowF = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    RainfallTotalInches = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    SnowTotalInches = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    ZipCode = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
