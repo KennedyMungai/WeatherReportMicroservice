7b8e4c0df1c4237de60f5bbf1f40c90357bbc9ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-18 17:50:34 +0300

    Created the service class that implements the IPrecipitation interface

---
+++ CloudWeather.Precipitation/Services/PrecipitationServices/PrecipitationService.cs
@@ -0,0 +1,11 @@
+
+namespace CloudWeather.Precipitation.Services.PrecipitationServices;
+
+
+public class PrecipitationService : IPrecipitationService
+{
+    public Task GetByZip(string zip, int? days)
+    {
+        throw new NotImplementedException();
+    }
+}
