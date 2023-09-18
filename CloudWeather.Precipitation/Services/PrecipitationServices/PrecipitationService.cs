87814247e73e4de6e9b09df618bd2bad830facb3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-18 17:51:44 +0300

    Added some db dependenc injection logic

--- CloudWeather.Precipitation/Services/PrecipitationServices/PrecipitationService.cs
+++ CloudWeather.Precipitation/Services/PrecipitationServices/PrecipitationService.cs
@@ -1,9 +1,18 @@
 
+using CloudWeather.Precipitation.DataAccess;
+
 namespace CloudWeather.Precipitation.Services.PrecipitationServices;
 
 
 public class PrecipitationService : IPrecipitationService
 {
+    private readonly PrecipDbContext _context;
+
+    public PrecipitationService(PrecipDbContext context)
+    {
+        _context = context;
+    }
+
     public Task GetByZip(string zip, int? days)
     {
         throw new NotImplementedException();
