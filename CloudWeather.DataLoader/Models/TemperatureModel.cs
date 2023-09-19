07e1e6e1cc24fafe51327cf77b4f38e5ca930b25
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 18:14:44 +0300

    Added properties to the temperature model

--- CloudWeather.DataLoader/Models/TemperatureModel.cs
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -1,7 +1,11 @@
+using System;
 namespace CloudWeather.DataLoader.Models;
 
 
 public class TemperatureModel
 {
-
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public string WeatherType { get; set; }
+    public string ZipCode { get; set; }
 }
