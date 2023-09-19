f854c933a75320e9018c44d7b9a6ba14526d1757
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 18:26:57 +0300

    Created a list of zip codes

--- CloudWeather.DataLoader/Program.cs
+++ CloudWeather.DataLoader/Program.cs
@@ -4,3 +4,22 @@ IConfiguration config = new ConfigurationBuilder()
                             .AddJsonFile("appsettings.json")
                             .AddEnvironmentVariables()
                             .Build();
+
+var servicesConfig = config.GetSection("Services");
+
+var tempServiceConfig = servicesConfig.GetSection("Temperature");
+var tempServiceHost = tempServiceConfig["Host"];
+var tempServicePort = tempServiceConfig["Port"];
+
+var precipServiceConfig = servicesConfig.GetSection("Precipitation");
+var precipServiceHost = precipServiceConfig["Host"];
+var precipServicePort = precipServiceConfig["Port"];
+
+List<string> zipCodes = new()
+{
+    "73026",
+    "68104",
+    "04401",
+    "32808",
+    "19717"
+};
