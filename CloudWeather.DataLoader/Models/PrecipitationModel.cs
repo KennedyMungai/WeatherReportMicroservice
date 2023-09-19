dba13abdd65b5ad1b1316e1ceda8d81a9f2625ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 18:13:10 +0300

    Added some properties to the precipitation model

--- CloudWeather.DataLoader/Models/PrecipitationModel.cs
+++ CloudWeather.DataLoader/Models/PrecipitationModel.cs
@@ -3,5 +3,8 @@ namespace CloudWeather.DataLoader.Models;
 
 public class PrecipitationModel
 {
-
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public string WeatherType { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
 }
