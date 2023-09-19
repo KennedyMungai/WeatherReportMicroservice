4d5680bd851d6353668d90706ea412faec133eee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 19:18:22 +0300

    Made some changes in the logic

--- CloudWeather.DataLoader/Models/TemperatureModel.cs
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -6,6 +6,6 @@ public class TemperatureModel
 {
     public DateTime CreatedOn { get; set; }
     public decimal AmountInches { get; set; }
-    public string WeatherType { get; set; }
-    public string ZipCode { get; set; }
+    public string WeatherType { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
 }
