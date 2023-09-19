82eef457d213f06cad28bdc8ee7305013c471aa8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 14:18:05 +0300

    Created the Precipitation model for the microservice

---
+++ CloudWeather.Report/Models/Precipitation.cs
@@ -0,0 +1,10 @@
+namespace CloudWeather.Report.Models;
+
+public class PrecipitationModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public string WeatherType { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+}
