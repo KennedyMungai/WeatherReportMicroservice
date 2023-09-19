dbdba2fd81381c05948f6203803ef604a093ba12
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 19:26:12 +0300

    Updated the Properties in the temperature model class

--- CloudWeather.DataLoader/Models/TemperatureModel.cs
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -5,7 +5,7 @@ namespace CloudWeather.DataLoader.Models;
 public class TemperatureModel
 {
     public DateTime CreatedOn { get; set; }
-    public decimal AmountInches { get; set; }
-    public string WeatherType { get; set; } = string.Empty;
+    public decimal TempHighF { get; set; }
+    public decimal TempLowF { get; set; }
     public string ZipCode { get; set; } = string.Empty;
 }
