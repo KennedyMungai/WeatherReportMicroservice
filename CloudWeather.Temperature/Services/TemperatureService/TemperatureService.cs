4566bca44e129d98a0d38339875b0b09224eb5fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-19 13:08:07 +0300

    Updated the model for the GetTemperatureData function

---
+++ CloudWeather.Temperature/Services/TemperatureService/TemperatureService.cs
@@ -0,0 +1,33 @@
+
+using CloudWeather.Temperature.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Temperature.Services.TemperatureService;
+
+
+public class TemperatureService : ITemperatureService
+{
+    private readonly TempDbContext _context;
+
+    public TemperatureService(TempDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<TemperatureModel>?> GetTemperatureData(string zip, int? days)
+    {
+        try
+        {
+            var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+            var results = await _context.TemperatureTable
+                                    .Where(temp => temp.ZipCode == zip)
+                                    .ToListAsync();
+            return results;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
