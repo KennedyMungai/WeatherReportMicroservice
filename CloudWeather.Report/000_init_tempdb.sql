181b7064c86be77aa21c246a4ee8cc0b5ebe7b3f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-20 14:59:10 +0300

    Scaffolded the sql scripts needed for migration of the data to the databases

---
+++ CloudWeather.Report/000_init_tempdb.sql
@@ -0,0 +1,62 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
+
+START TRANSACTION;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918164633_InitialMigration') THEN
+
+    ALTER DATABASE CHARACTER SET utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918164633_InitialMigration') THEN
+
+    CREATE TABLE `Reports` (
+        `Id` char(36) COLLATE ascii_general_ci NOT NULL,
+        `CreatedOn` datetime(6) NOT NULL,
+        `AverageHighF` decimal(65,30) NOT NULL,
+        `AverageLowF` decimal(65,30) NOT NULL,
+        `RainfallTotalInches` decimal(65,30) NOT NULL,
+        `SnowTotalInches` decimal(65,30) NOT NULL,
+        `ZipCode` longtext CHARACTER SET utf8mb4 NOT NULL,
+        CONSTRAINT `PK_Reports` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918164633_InitialMigration') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20230918164633_InitialMigration', '7.0.11');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
---
+++ CloudWeather.Temperature/000_init_tempdb.sql
@@ -0,0 +1,60 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
+
+START TRANSACTION;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918163539_InitialMigration') THEN
+
+    ALTER DATABASE CHARACTER SET utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918163539_InitialMigration') THEN
+
+    CREATE TABLE `TemperatureTable` (
+        `Id` char(36) COLLATE ascii_general_ci NOT NULL,
+        `CreatedOn` datetime(6) NOT NULL,
+        `TempHighF` decimal(65,30) NOT NULL,
+        `TempLowF` decimal(65,30) NOT NULL,
+        `ZipCode` longtext CHARACTER SET utf8mb4 NOT NULL,
+        CONSTRAINT `PK_TemperatureTable` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20230918163539_InitialMigration') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20230918163539_InitialMigration', '7.0.11');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
